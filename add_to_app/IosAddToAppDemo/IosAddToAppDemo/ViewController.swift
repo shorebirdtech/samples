9c3902f93089aa0873ce21154afb620fcefb9e52
Bryan Oltman <bryan@shorebird.dev>
2023-07-28 17:43:25 -0400

    chore: add new Xcode project

---
+++ add_to_app/IosAddToAppDemo/IosAddToAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IosAddToAppDemo
+//
+//  Created by Bryan Oltman on 7/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
