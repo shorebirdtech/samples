772398608d6e38227def29b028a44d3e4aa0fda4
Bryan Oltman <bryan@shorebird.dev>
2023-09-08 11:25:04 -0400

    initial creation of new xcode project

---
+++ add_to_app/IosAddToAppDemoCocoapods/IosAddToAppDemoCocoapods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IosAddToAppDemoCocoapods
+//
+//  Created by Bryan Oltman on 9/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
