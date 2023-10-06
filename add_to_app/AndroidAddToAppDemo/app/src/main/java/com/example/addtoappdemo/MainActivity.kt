1d3326e740acd98db780da05d86dcb5c15541417
Bryan Oltman <bryan@shorebird.dev>
2023-10-06 13:54:29 -0400

    feat(add_to_app): improve android hybrid app example

--- add_to_app/AndroidAddToAppDemo/app/src/main/java/com/example/addtoappdemo/MainActivity.kt
+++ add_to_app/AndroidAddToAppDemo/app/src/main/java/com/example/addtoappdemo/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.addtoappdemo.databinding.ActivityMainBinding
-import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,28 +28,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener {
-            startActivity(
-                FlutterActivity.createDefaultIntent(this)
-            )
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
